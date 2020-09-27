9ea224bcec2708dc6bb6718dcea0de93a90ca854
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2020-09-27 10:08:46 -0700

    indentation in helloworld.h

--- src/helloworld.h
+++ src/helloworld.h
@@ -5,17 +5,16 @@
 #include <gtkmm/window.h>
 
 class HelloWorld : public Gtk::Window {
+	public:
+		HelloWorld();
+		virtual ~HelloWorld();
 
-public:
-	HelloWorld();
-	virtual ~HelloWorld();
+	protected:
+		// Signal handlers:
+		void on_button_clicked();
 
-protected:
-	// Signal handlers:
-	void on_button_clicked();
-
-	// Member widgets:
-	Gtk::Button m_button;
+		// Member widgets:
+		Gtk::Button m_button;
 };
 
 #endif // GTKMM_EXAMPLE_HELLOWORLD_H
