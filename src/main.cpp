3f599a4a885bba5e88334121433cb43de709e175
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2020-11-23 19:46:51 -0800

    add basic glade example from https://gitlab.gnome.org/GNOME/gtkmm-documentation/-/blob/master/examples/book/builder/basic/main.cc original source is GPL2 licensed

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,60 @@
-#include "importwindow.h"
-#include <gtkmm/application.h>
+#include <gtkmm.h>
+#include <iostream>
 
-int main(int argc, char *argv[]) {
-	auto app = Gtk::Application::create(argc, argv, "com.adriancedwards.reach");
+namespace {
+Gtk::Dialog *pDialog = nullptr;
+Glib::RefPtr<Gtk::Application> app;
 
-	ImportWindow helloworld;
+void on_button_clicked() {
+	if (pDialog)
+		pDialog->hide(); // hide() will cause Gtk::Application::run() to end.
+}
 
-	// Shows the window and returns when it is closed.
-	return app->run(helloworld);
+void on_app_activate() {
+	// Load the GtkBuilder file and instantiate its widgets:
+	auto refBuilder = Gtk::Builder::create();
+	try {
+		refBuilder->add_from_file("reach.glade");
+	} catch (const Glib::FileError &ex) {
+		std::cerr << "FileError: " << ex.what() << std::endl;
+		return;
+	} catch (const Glib::MarkupError &ex) {
+		std::cerr << "MarkupError: " << ex.what() << std::endl;
+		return;
+	} catch (const Gtk::BuilderError &ex) {
+		std::cerr << "BuilderError: " << ex.what() << std::endl;
+		return;
+	}
+
+	// Get the GtkBuilder-instantiated dialog:
+	pDialog = refBuilder->get_widget<Gtk::Dialog>("DialogBasic");
+	if (!pDialog) {
+		std::cerr << "Could not get the dialog" << std::endl;
+		return;
+	}
+
+	// Get the GtkBuilder-instantiated button, and connect a signal handler:
+	auto pButton = refBuilder->get_widget<Gtk::Button>("quit_button");
+	if (pButton)
+		pButton->signal_clicked().connect([]() { on_button_clicked(); });
+
+	// It's not possible to delete widgets after app->run() has returned.
+	// Delete the dialog with its child widgets before app->run() returns.
+	pDialog->signal_hide().connect([]() { delete pDialog; });
+
+	app->add_window(*pDialog);
+	pDialog->show();
+}
+} // anonymous namespace
+
+int main(int argc, char **argv) {
+	app = Gtk::Application::create("com.adriancedwards.reach");
+
+	// Instantiate a dialog when the application has been activated.
+	// This can only be done after the application has been registered.
+	// It's possible to call app->register_application() explicitly, but
+	// usually it's easier to let app->run() do it for you.
+	app->signal_activate().connect([]() { on_app_activate(); });
+
+	return app->run(argc, argv);
 }
