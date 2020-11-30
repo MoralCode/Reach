b579715adda22b92c91b85a19184c6c28c0d848b
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2020-11-29 20:24:51 -0800

    commit gui.h

---
+++ src/gui.h
@@ -0,0 +1,25 @@
+#ifndef GTKMM_GUI_H
+#define GTKMM_GUI_H
+#include <gtkmm.h>
+#include <gtkmm/application.h>
+
+namespace G {
+	class ReachGUI {
+	public:
+		ReachGUI(std::string appid);
+		// virtual ~ReachGUI();
+		Glib::RefPtr<Gtk::Application> app;
+
+	protected:
+		// Signal handlers:
+		void close_hide_window();
+		void on_app_activate();
+
+	// 	// Child widgets:
+	// 	// Gtk::ButtonBox m_ButtonBox;
+	// 	// Gtk::Button m_Button_File, m_Button_Folder;
+
+	};
+} // namespace G
+
+#endif // GTKMM_GUI_H
