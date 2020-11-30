d5e94d4f280a32e298a0f1b19e5590ad58862545
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2020-11-29 16:56:09 -0800

    register resources file so it is accessible as a global resource

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,16 @@
 #include "gui.h"
 #include <libconfig.h++>
+extern "C" {
+#include "ui.gresource.h"
+}
 
 int main(int argc, char **argv) {
+
+	// register the resource bundle
+	auto resource_bundle = Glib::wrap(ui_get_resource());
+	// std::cerr << "rp: " << resource_bundle << std::endl;
+	resource_bundle->register_global();
+
 	G::ReachGUI gui("com.adriancedwards.reach");
 
 	return gui.app->run(argc, argv);
