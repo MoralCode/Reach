4fb492598db8f5d0aca9681ff15fa93375849867
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2021-01-21 20:25:01 -0800

    add fileManager class for storing contacts in a directory

---
+++ src/app/filemanager.cpp
@@ -0,0 +1,29 @@
+#include "filemanager.h"
+#include <iostream>
+#include <libconfig.h++>
+
+#define CONFIG_FILENAME "reach.cfg"
+
+using namespace A;
+using namespace libconfig;
+
+FileManager::FileManager(std::string path = "db") {
+	setup_metadata();
+}
+
+void FileManager::setup_metadata() {
+	try {
+		metadata.readFile(CONFIG_FILENAME);
+	} catch (const FileIOException &fioex) {
+		std::cerr << "I/O error while reading file." << std::endl;
+		// return (EXIT_FAILURE);
+	} catch (const ParseException &pex) {
+		std::cerr << "Parse error at " << pex.getFile() << ":" << pex.getLine()
+				  << " - " << pex.getError() << std::endl;
+		// return (EXIT_FAILURE);
+	}
+}
+
+vector<string> list_filenames() {
+	// http://www.martinbroadhurst.com/list-the-files-in-a-directory-in-c.html
+}
---
+++ src/app/filemanager.h
@@ -0,0 +1,30 @@
+#ifndef REACH_FILEMANAGER_H
+#define REACH_FILEMANAGER_H
+#include <string>
+#include <vector>
+
+using namespace std;
+
+namespace A {
+class FileManager {
+	//manages the on-disk file structure of the contacts as a sort of DIY database
+public:
+	FileManager(string path);
+	// virtual ~FileManager();
+	FILE * get_file(string filename);
+	void delete_file(string filename);
+	vector<string> list_filenames();
+
+
+protected:
+	//sets up a metadata file explaining the database and its structure
+	void setup_metadata();
+
+private:
+	string db_root_path;
+	Config metadata;
+
+};
+} // namespace A
+
+#endif // REACH_FILEMANAGER_H
