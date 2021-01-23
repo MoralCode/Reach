60c1341737854e6d120c52f3cf31944ec68b1c8a
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2021-01-22 23:33:20 -0800

    properly initialize Field class values

--- src/model/field.cpp
+++ src/model/field.cpp
@@ -4,8 +4,4 @@
 #include "field.h"
 using namespace std;
 
-Field :: Field(string type, string label, string value) {
-	vc_type = type;
-	label = label;
-	value = value;
-}
+Field::Field(string type, string label, string value) : vc_type(type), label(label), value(value) {}
